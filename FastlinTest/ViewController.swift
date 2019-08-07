ac5b15534266644fb84d3588d30ec3b1c165ef2c
tirupathiAmz <tirupathi.mandali@amzurtech.com>
2019-08-07 22:23:54 +0530

    Initial Commit

---
+++ FastlinTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastlinTest
+//
+//  Created by Tirupathi Mandali on 8/7/19.
+//  Copyright © 2019 Amzur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
